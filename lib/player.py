421e2a522259da827092059dc7275663120822d0
Matt Poegel <mpoegel@comcast.net>
2015-04-19 01:12:45 -0400

    simple get move ai and player wrapper class

---
+++ lib/player.py
@@ -0,0 +1,23 @@
+# ==============================================================================
+# TwitchPlaysCheckers
+# HackRU Spring 2015 -- 4/18/2015
+#
+# Player Wrapper Class
+# ==============================================================================
+
+class Player(object):
+	# CONSTRUCTOR
+	def __init__(self, name, moveFunc):
+		self.__name = name
+		self.getMove = moveFunc
+	# ACCESSORS
+	def getName(): return self.__name
+	# MODIFIERS
+	
+
+
+
+# ------------------------------------------------------------------------------
+if (__name__ == '__main__'):
+
+	pass
