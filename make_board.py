4518374a3cd67eb42a0b8029ac802ad558ebe373
Batboa <andrewbatbouta@gmail.com>
2015-04-19 01:58:59 -0400

    board editing is now function

--- make_board.py
+++ make_board.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import math as m
+#adds point to board
 def add_piece(x_,y_, board, piece):
-    white = [255,255,255]
+    if piece == None:
+        return board
     rgb = np.asarray([piece[:,:,0], piece[:,:,1], piece[:,:,2]])
-    mask = (rgb[0] < 230) & (rgb[1] < 230) & (rgb[2] < 230)
+    mask = (rgb[0] != 255) & (rgb[1] != 255) & (rgb[2] != 255)
 
     x = board.shape[0]/8
     y = board.shape[1]/8
@@ -17,24 +19,39 @@ def add_piece(x_,y_, board, piece):
                 board[i, j] = piece[i-x_start, j-y_start]
     return board
 
-
-
-board = cv2.imread("images/board.jpg")
-board = cv2.cvtColor(board, cv2.COLOR_BGR2RGB)
-red = cv2.imread("images/red.jpg")
-red = cv2.cvtColor(red, cv2.COLOR_BGR2RGB)
-x = board.shape[0]/8
-y = board.shape[1]/8
-
-
-
-black = cv2.imread("images/black.jpg")
-for i in range(8):
-    for j in range(8):
-        board = add_piece(i,j, board, red)
-sigma = 1
-ksize = (2*sigma+1,2*sigma+1)
-board = cv2.GaussianBlur(board, ksize, sigma)
-plt.imshow(board)
-plt.axis("off")
-plt.show()
+def drawBoard(board):
+	#Reads in images
+	board = cv2.imread("images/board.jpg")
+	red = cv2.imread("images/red.png")
+	black = cv2.imread("images/black.png")
+	red_king = cv2.imread("images/red_king.png")
+	black_king = cv2.imread("images/black_king.png")
+	
+	#sets counters to iterate through board
+	x = 0
+	y = 0
+	none = 0
+	#Looping through given board state and makes board
+	for row in board:
+		y = 0
+		for piece in row:
+			if piece == None:
+				none  = 0
+			elif piece == "a":
+				board = add_piece(x,y, board, red)
+			elif piece == "A":
+				board = add_piece(x,y, board, red_king)
+			elif piece == "b":
+				board = add_piece(x,y, board, black)
+			elif piece == "B":
+				board = add_piece(x,y, board, black_king)
+			#Unexpected input from board input
+			else:
+				print "Invalid input"
+			y+=1
+		x+=1
+		
+	sigma = 2
+	ksize = (2*sigma+1,2*sigma+1)
+	board = cv2.GaussianBlur(board, ksize, sigma)
+	cv2.imwrite("images/current.png", board)
