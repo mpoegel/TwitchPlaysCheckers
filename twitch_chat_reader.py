56c5a6e74283e1e63014439e001ca1f46bd425c5
Ariel <leea10@rpi.edu>
2015-04-19 00:02:34 -0400

    Wrap twitch chat reader with an object

--- twitch_chat_reader.py
+++ twitch_chat_reader.py
@@ -1,43 +1,55 @@
 import socket
 import sys
 
-server = "irc.twitch.tv"
-password = "oauth:8nq3rymmy4zakeut4p90hx9axnpywj"
-nickname = "twitchlosescheckers"
-channel = "#twitchlosescheckers"
+class TwitchChatReader():
+	def __init__(self):
+		self.server = "irc.twitch.tv"
+		self.password = "oauth:8nq3rymmy4zakeut4p90hx9axnpywj"
+		self.nickname = "twitchlosescheckers"
+		self.channel = "#twitchlosescheckers"
+		self.irc = None
 
-try:
-	irc = socket.socket()
-except socket.error as msg:
-	irc = None
+		try:
+			self.irc = socket.socket()
+		except socket.error as msg:
+			self.irc = None
 
-try:
-	irc.connect((server, 6667))
-	print "connected to %s" %(server)
-except socket.error as msg:
-	irc.close()
-	irc = None
+		try:
+			self.irc.connect((self.server, 6667))
+		except socket.error as msg:
+			self.irc.close()
+			self.irc = None
 
-if irc is None:
-	print "could not open socket"
-	sys.exit()
+		if self.irc is None:
+			print "could not open socket"
+			sys.exit()
 
-irc.send('PASS ' + password + '\n')
-irc.send('NICK ' + nickname + '\n')
-irc.send('JOIN ' + channel + '\n')
+		self.irc.send('PASS ' + self.password + '\n')
+		self.irc.send('NICK ' + self.nickname + '\n')
+		self.irc.send('JOIN ' + self.channel + '\n')
 
-while True:
-	twitch_activity = irc.recv(4096)
-	print twitch_activity
-	
-	if "PING" in twitch_activity:
-		irc.send("PONG tmi.twitch.tv");
+	def watch_chat(self):
+		while True:
+			twitch_activity = self.irc.recv(4096)
+			print twitch_activity
+			
+			if twitch_activity.isspace():
+				return
 
-	# If the channel has receieved a twitch chat message
-	if "PRIVMSG" in twitch_activity:
+			if "PING" in twitch_activity:
+				self.irc.send("PONG tmi.twitch.tv")
+				print "PONG tmi.twitch.tv"
 
-		# extract the contents of the message
-		i = twitch_activity[1:].find(":")
-		message = twitch_activity[i+2:]
+			# If the channel has receieved a twitch chat message
+			if "PRIVMSG" in twitch_activity:
 
-irc.close()
+				# extract the contents of the message
+				i = twitch_activity[1:].find(":")
+				message = twitch_activity[i+2:]
+				command = message.split()
+
+#################### MAIN - TEST ############################
+
+if __name__ == '__main__':
+	userInput = TwitchChatReader()
+	userInput.watch_chat()
